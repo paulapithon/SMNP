0ab24780f8496caf111d236512999165fa025f97
root <root@DESKTOP-MA6K2FJ.localdomain>
2018-11-08 19:43:08 -0300

    Adiciona o DAtagram socket

---
+++ Socket/DatagramSocket.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Pichau on 08/11/2018.
+//
+
---
+++ Socket/DatagramSocket.h
@@ -0,0 +1,8 @@
+//
+// Created by Pichau on 08/11/2018.
+//
+
+#ifndef CLIENT_DATAGRAMSOCKET_H
+#define CLIENT_DATAGRAMSOCKET_H
+
+#endif //CLIENT_DATAGRAMSOCKET_H
